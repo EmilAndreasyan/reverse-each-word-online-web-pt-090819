8630f8edace995a04ef90e614d6d66f832327fa7
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-17 05:43:09 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+arr = "Hello there, and how are you?"
+def reverse_each_word(arr)
+  return_array = []
+  string = arr.split(" ")
+  string.each do |word|
+return_array << word.reverse
+  end
+  return_array.join(" ")
+  end
+  reverse_each_word(arr)
+  
+  
+#   def reverse_each_word(arr)
+#     string = arr.split(" ")
+#     string.collect do |word|
+#       string << word.reverse
+#       string.join(" ")
+#   end
+# end
+# reverse_each_word(arr)
